cdc1b4df5b2acf03834b13725ef24201ce054eca
Shannen <shannen.ye@gmail.com>
2021-04-23 23:25:57 -0400

    Adding test for ApiService

--- src/app/api/api.service.spec.ts
+++ src/app/api/api.service.spec.ts
@@ -1,16 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { ticketDataList } from '../dashboard/data';
 
 import { ApiService } from './api.service';
 
-describe('DashboardService', () => {
-  let service: ApiService;
+describe('ApiService', () => {
+    let service: ApiService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ApiService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should return an array of TicketDetail objects', () => {
+        expect(service.getDashboardData()).toBe(ticketDataList);
+    })
 });
